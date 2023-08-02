329f81c9520c928310ee80853c24e56189a8bec8
lkdcode <dlrlejr1@naver.com>
2023-08-02 15:39:46 +0900

    [feat] securityconfig initial commit

---
+++ src/main/java/com/example/wantedbackend/global/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.wantedbackend.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+        return httpSecurity.build();
+    }
+
+}
