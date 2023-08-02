7a7fda9417a62cd6820d02e5e1e095aab447c5b8
lkdcode <dlrlejr1@naver.com>
2023-08-02 15:18:34 +0900

    [feat] Initial commit for post layout

---
+++ src/main/java/com/example/wantedbackend/module/Post/controller/dto/response/PostUpdateResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.wantedbackend.module.Post.controller.dto.response;
+
+public class PostUpdateResponseDTO {
+}
---
+++ src/main/java/com/example/wantedbackend/module/Post/controller/mapper/PostMapper.java
@@ -0,0 +1,8 @@
+package com.example.wantedbackend.module.Post.controller.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public class PostMapper {
+
+}
