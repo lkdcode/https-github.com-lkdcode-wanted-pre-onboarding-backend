1ed44291265f0cadf07d1dfb1cff5f325a7751ad
lkdcode <dlrlejr1@naver.com>
2023-08-15 23:15:59 +0900

    [feat] post : Add command api

--- src/main/java/com/example/wantedbackend/post/api/dto/PostCommandDTO.java
+++ src/main/java/com/example/wantedbackend/post/api/dto/PostCommandDTO.java
@@ -1,8 +1,10 @@
 package com.example.wantedbackend.post.api.dto;
 
+import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public record PostCommandDTO() {
 
@@ -16,12 +18,27 @@ public record PostCommandDTO() {
     ) {
     }
 
+    @Builder
     public record PostCreateResponseDTO(Boolean success) {
     }
 
+    @Builder
     public record PostDeleteResponseDTO(Boolean success) {
     }
 
+    public record PostUpdateRequestDTO(
+            @NotNull
+            Long postId,
+            @NotBlank
+            @Length(max = 50)
+            String updateTitle,
+            @NotBlank
+            @Length(max = 2000)
+            String updateContent
+    ) {
+    }
+
+    @Builder
     public record PostUpdateResponseDTO(Boolean success) {
     }
 
--- src/main/java/com/example/wantedbackend/post/service/PostQueryUsecase.java
+++ src/main/java/com/example/wantedbackend/post/service/PostQueryUsecase.java
@@ -4,11 +4,14 @@ package com.example.wantedbackend.post.service;
 import com.example.wantedbackend.post.api.dto.PostQueryDTO.PostFindAllResponseDTO;
 import com.example.wantedbackend.post.api.dto.PostQueryDTO.PostResponseDTO;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PostQueryUsecase {
 
+    @Transactional(readOnly = true)
     PostFindAllResponseDTO findAll(Pageable pageable);
 
+    @Transactional(readOnly = true)
     PostResponseDTO findById(Long id);
 
 }
